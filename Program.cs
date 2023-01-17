c56ef60251ddba3cf0e22e169fdadefaffe1ce33
Matheus <matheusdsc57@gmail.com>
2023-01-17 10:32:52 -0400

    Finalizado

--- Program.cs
+++ Program.cs
@@ -7,12 +7,44 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 1;
+
+            if (type == 'm') multiplier = 60;
+
+            if (time == 0) System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!!!");
+            Thread.Sleep(2000);
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -22,6 +54,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado...");
+            Thread.Sleep(2000);
+            Menu();
         }
     }
 }
